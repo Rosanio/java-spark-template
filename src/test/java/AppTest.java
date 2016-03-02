01613e6bb03661293d5c192232b94a4c14f58f21
Jake Steinberg and Matt Rosanio <Jakesteinberg@gmail.com>
2016-03-01 16:37:26 -0800

    Add DB.java, DatabaseRule.java, and modify build.gradle, AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
